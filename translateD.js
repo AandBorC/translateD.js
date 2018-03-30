47d80ce3fc52e2721e5af87622d640520be43cac
mianxiu <mianxiu8481@gmail.com>
2018-03-31 00:34:25 +0800

    ver 5.0

--- translateD.js
+++ translateD.js
@@ -23,43 +23,66 @@
     let COLOR = '#7373f3'
 
     let translateDCSSContent = ''
+        + '#translateD a {text-decoration: none;}'
+        + '#translateD h1{font-size:26px;}'
+        + '#translateD h2{font-size:19.5px;margin:10.70px 0px;}'
+        + '#translateD h3{font-size:18px}'
+        + '#translateD h4{font-size:17.29px;}'
+        + '#translateD h5{font-size:10.79px;}'
+        + '#translateD h6{font-size:8.71px;}'
+        + '#translateD h1,#translateD h2{font-weight:bold;}'
+        + '#translateD p{margin:13px 0px;}'
         + '#translateD h1,#translateD h2,#translateD h3,#translateD h4,#translateD h5,#translateD h6{margin-top:0;}'
         + '#translateD li{ list-style:none;}'
+        + '#translateD dl{margin:13px 0px;}'
         + '#translateD dd{ margin-left:6px;}'
         + '#translateD dd ul{ margin-left:6px;padding-right: 6px;}'
         + '#translateD {overflow:hidden;text-align:left;font-family:"Microsoft YaHei",Arial, Helvetica, sans-serif;z-index:99999;position:fixed;width:' + WIDTH + 'px;height:96vh;top:1.8vh;right:1.8vh;border-radius:4px;border:1px solid #7575cc;background-color:white;opacity:0.96;box-shadow:0px 0px 30px #00000047;}'
-        + '#translateD .content{padding:0;border:0;}'
+        + '#translateD .translateD-content{padding:0;border:0;}'
         // 分类
-        + '.word-nav{position:absolute;font-size:14px;padding-left:1px;min-width:' + WIDTH + 'px;height:34px;align-items:center;background-color:white;display:flex;list-style:none;border-top:1px solid #efefef;border-bottom:1px solid #efefef;margin-left:0px;transition:margin-left 0.4s;}'
-        + '.word-nav li{padding-left:12px;width:62px;}'
-        + '.redirection{color:#ffffffd9;border-radius:4px;padding:12px;background-color:#00000014;}'
-        + '.word-details-pane-header{padding:1.8vh;border-top-left-radius:2px;border-top-right-radius:2px;background-color:' + COLOR + ';color:white;}'
-        + '#word-details-pane-content{overflow-y:hidden;}'
-        + '.word-details-pane-content{font-size:12px;padding:12px;padding-top:6px;transition:margin-top 0.2s;}'
+        + '.word-nav {position:absolute;font-size:14px;padding-left:1px;min-width:' + WIDTH + 'px;height:34px;align-items:center;background-color:white;display:flex;list-style:none;border-top:1px solid #efefef;border-bottom:1px solid #efefef;margin-left:0px;transition:margin-left 0.4s;}'
+        + '.word-nav li {padding-left:12px;width:62px;}'
+        + '.word-nav a {color:black;}'
+        + '.redirection {color:#ffffffd9;border-radius:4px;padding:12px;background-color:#00000014;}'
+        + '.word-details-pane-header {padding:1.8vh;border-top-left-radius:2px;border-top-right-radius:2px;background-color:' + COLOR + ';color:white;}'
+        + '#word-details-pane-content {overflow-y:hidden;}'
+        + '.word-details-pane-content {font-size:12px;padding:12px;padding-top:6px;transition:margin-top 0.2s;}'
         + '.word-details-pane-content > div:nth-child(n+2) {display:none;}'
-        + '.word-nav-point{display:block;position:absolute;width:62px;height:2px;background-color:' + COLOR + ';transition:margin-left 0.4s;margin-top:34px;}'
+        + '.word-nav-point {display:block;position:absolute;width:62px;height:2px;background-color:' + COLOR + ';transition:margin-left 0.4s;margin-top:34px;}'
         // 来源词典与分级去除
-        + '.detail-source{display:none;}'
+        + '.detail-source {display:none;}'
         + '.collins-icon {padding:2px;background-color:#e84b4b;color:white;}'
-        + '.wys-icon,.wjs-icon{padding:2px;background-color:' + COLOR + ';color:white;}'
-        + '.detail-tags-en{display:none;}'
-        + '.detail-tags-en li{padding:2px 4px;background-color:#f1f1f1;list-style:none;margin-right:4px;}'
-        + '.def-sentence-from{margin-bottom:4px;}'
-        + '.def-sentence-to{margin-top:0px;color:#8a8a8a;}'
-        + '.detail-groups > dl{border-bottom:1px solid #f1f1f1;}'
-        + '.detail-groups > dl dt,.enen-groups >dl dt{color:' + COLOR + '; padding:2px 6px 4px 6px;font-size:16px;border-radius:4px; background-color:#f8f8f8;border:1px solid #e6e6e6;}'
-        + '.detail-groups > dl dt span.detail-pron{color:#b5b5b5;}'
+        + '.wys-icon,.wjs-icon {padding:2px;background-color:' + COLOR + ';color:white;}'
+        + '.detail-tags-en {display:none;}'
+        + '.detail-tags-en li {padding:2px 4px;background-color:#f1f1f1;list-style:none;margin-right:4px;}'
+        + '.def-sentence-from {margin-bottom:4px;}'
+        + '.def-sentence-to {margin-top:0px;color:#8a8a8a;}'
+        + '.detail-groups > dl {border-bottom:1px solid #f1f1f1;}'
+        + '.detail-groups > dl dt,.enen-groups >dl dt,.analyzes-title {overflow-wrap: break-word;color:' + COLOR + '; padding:2px 6px 4px 6px;font-size:16px;border-radius:4px; background-color:#f8f8f8;border:1px solid #e6e6e6;}'
+        + '.detail-groups > dl dt span.detail-pron {color:#b5b5b5;}'
         + '.enen-groups >dl > dd {font-size:14px;;margin-top:10px;margin-bottom:10px;}'
         // counter CSS计数器，给无序tag增加序列
-        + '.detail-groups dd h3{font-size:14px;color:#7979f3}'
-        + '.detail-groups dl dd h3:before{display:inline;width:22px;}'
-        + '.detail-groups dl > dd:nth-child(1) > p{color:red;}'
-        + '.phrase-items li{margin-bottom:10px;}'
-        + '.phrase-items span:nth-child(1){font-size:14px;color:' + COLOR + ';display:block;}'
-        + '.syn table{background-color:#f8f8f8;border:1px solid #e6e6e6;border-radius:4px;border-spacing:0px;}'
-        + '.syn tr:nth-child(2n) {background-color:white;}'
-        + '.syn tr td{padding:6px;width:'+ (WIDTH-24)/2+'px; cursor:pointer;}'
-        + '.syn tr > td:nth-child(1){border-right:1px solid #e6e6e6;}'
+        + '.detail-groups h3 {font-size:14px!important;color:' + COLOR + '}'
+        + '.detail-groups dl dd h3:before {display:inline;width:22px;}'
+        + '.detail-groups dl > dd:nth-child(1) > p {color:red;}'
+        + '.phrase-items li {margin-bottom:10px;}'
+        + '.phrase-items span:nth-child(1)  {font-size:14px;color:' + COLOR + ';display:block;}'
+        + '.syn table,.ant table{background-color:#f8f8f8;border:1px solid #e6e6e6;border-radius:4px;border-spacing:0px;}'
+        + '.syn tr:nth-child(2n),.ant tr:nth-child(2n) {background-color:white;}'
+        + '.syn tr td,.ant tr td {padding:6px;width:' + (WIDTH - 24) / 2 + 'px; cursor:pointer;transition:background-color 0.2s,color 0.2s;}'
+        + '.syn tr td:hover,.ant tr td:hover {background-color:' + COLOR + ';}'
+        + '.syn tr td a,.ant tr td a {color:black;transition:color 0.2s;}'
+        + '.syn tr td:hover a,.ant tr td:hover a {color:white!important;}'
+        + '.syn tr > td:nth-child(1),.ant tr > td:nth-child(1) {border-right:1px solid #e6e6e6;}'
+        + '.analyzes-items li a {font-size:14px;color:' + COLOR + '}'
+        + '.inflections-items {font-size:14px;padding:10px 0px;}'
+        + '.inflections-items li {margin-bottom:16px;}'
+        + '.inflections-item-attr {padding:4px;background-color:' + COLOR + ';color:white;border-radius:4px 0px 0px 4px;}'
+        + '.inflections-items a {padding:4px 8px;background-color:#f8f8f8;color:black;border-radius:0px 4px 4px 0px;}'
+
+        + '.simple-definition a{display:inline-block;margin-top:10px;margin-right:2px;cursor:pointer;text-decoration:none;color:white;padding:4px 8px;border:1px solid #ffffff14;border-radius:4px;background-color:#ffffff24;transition:background-color 0.2s;}'
+        + '.simple-definition a:hover{background-color:#ffffff63;}'
+
 
 
 
@@ -110,12 +133,14 @@
                         .replace(/<div class="word-details-ads-placeholder"><\/div>/, '<div id="word-nav">' + result.match(/<ul class="word-nav">.+?<\/ul>/gm) + '<span class="word-nav-point" data-point = "1" style="margin-left:10px;"></span></div>')
                         .replace(/<ul class="word-nav">.+?<\/ul>/, '')
                         .replace(/<ul class="word-nav">/, '<ul class="word-nav" style="margin-left:0px;">')
-                        .replace(/<footer.+<\/footer>/gm, '')
                         .replace(/<div class="word-details-ads">.+?<\/div>\s.<\/div>/gm, '')
                         .replace(/<div class="word-details-pane-content".+<\/div>(?=\s.+<\/section>\s.)/gm, '<div id="word-details-pane-content">' + result.match(/<div class="word-details-pane-content".+<\/div>(?=\s.+<\/section>\s.)/gm) + '</div>')
                         .replace(/<div class="word-details-pane-content"/gm, '<div class="word-details-pane-content" style="margin-top:36px";')
                         .replace(/(<h2>网络热点<\/h2>)|(<h2>详细释义<\/h2>)|(<h2>常用短语<\/h2>)|(<h2>英英释义<\/h2>)|(<h2>词形变化<\/h2>)|(<h2>同反义词<\/h2>)|(<h2>词义辨析<\/h2>)/gm, '')
                         .replace(/<a\shref="\/w\//gm, '<a data-syn-word="')
+                        .replace(/http:\/\//gm, 'https://')
+                        .replace(/class="content"/g, 'class="translateD-content"')
+                        .replace(/<footer.+<\/footer>/gm, '')
                 )
 
 
@@ -265,13 +290,25 @@
 
                 let wordSyn = function () {
                     // 同反义词跳转
-                    if(document.getElementsByClassName('syn').length !== 0){
-                        document.querySelector('.syn').addEventListener('click', e => {
-                            if(e.target.tagName == 'A'){
-                                foo(e.target.getAttribute('data-syn-word'),en)
-                            }
+                    let aTag = function () {
+                        if (event.target.tagName == 'A') {
+                            foo(event.target.getAttribute('data-syn-word'), en)
+                        } else if (event.target.tagName == 'TD') {
+                            foo(event.target.childNodes[1].getAttribute('data-syn-word'), en)
+                        }
+                    }
+
+                    if (document.getElementsByClassName('syn').length !== 0) {
+                        document.querySelector('.syn').addEventListener('click', event => {
+                            aTag()
                         })
-                    }     
+                    }
+
+                    if (document.getElementsByClassName('simple-definition').length !== 0) {
+                        document.querySelector('.simple-definition').addEventListener('click', event => {
+                            aTag()
+                        })
+                    }
                 }
 
                 wordSyn()
