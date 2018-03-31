34ba57fdd1023ae361704d4bd627ea41c10bd3cf
mianxiu <mianxiu8481@gmail.com>
2018-03-31 21:59:52 +0800

    ver 5.0

--- translateD.js
+++ translateD.js
@@ -41,13 +41,15 @@
         + '#translateD .translateD-content{padding:0;border:0;}'
         // 分类
         + '.word-nav {position:absolute;font-size:14px;padding-left:1px;min-width:' + WIDTH + 'px;height:34px;align-items:center;background-color:white;display:flex;list-style:none;border-top:1px solid #efefef;border-bottom:1px solid #efefef;margin-left:0px;transition:margin-left 0.4s;}'
-        + '.word-nav li {padding-left:12px;width:74px;}'
+        + '.word-nav li {padding-left:12px;width:62px;}'
         + '.word-nav a {color:black;}'
         + '.redirection {color:#ffffffd9;border-radius:4px;padding:12px;background-color:#00000014;}'
         + '.word-details-pane-header {padding:1.8vh;border-top-left-radius:2px;border-top-right-radius:2px;background-color:' + COLOR + ';color:white;}'
-        + '#word-details-pane-content {overflow-y:hidden;}'
+        + '#word-details-pane-content-all {overflow-y:hidden;}'
         + '.word-details-pane-content {font-size:12px;padding:12px;padding-top:6px;transition:margin-top 0.2s;}'
         + '.word-details-pane-content > div:nth-child(n+2) {display:none;}'
+        + '.word-details-pane:nth-child(n+2) {display:none;}'
+        + '.word-details-pane-content-all{overflow:hidden;}'
         + '.word-nav-point {display:block;position:absolute;width:62px;height:2px;background-color:' + COLOR + ';transition:margin-left 0.4s;margin-top:34px;}'
         // 来源词典与分级去除
         + '.detail-source {display:none;}'
@@ -66,7 +68,7 @@
         + '.detail-groups dl dd h3:before {display:inline;width:22px;}'
         + '.detail-groups dl > dd:nth-child(1) > p {color:red;}'
         + '.phrase-items li {margin-bottom:10px;}'
-        + '.phrase-items span:nth-child(1)  {font-size:14px;color:' + COLOR + ';display:block;}'
+        + '.phrase-items span:nth-child(1),.phrase-items a  {font-size:14px;color:' + COLOR + ';display:block;}'
         + '.syn table,.ant table{background-color:#f8f8f8;border:1px solid #e6e6e6;border-radius:4px;border-spacing:0px;}'
         + '.syn tr:nth-child(2n),.ant tr:nth-child(2n) {background-color:white;}'
         + '.syn tr td,.ant tr td {padding:6px;width:' + (WIDTH - 24) / 2 + 'px; cursor:pointer;transition:background-color 0.2s,color 0.2s;}'
@@ -103,6 +105,7 @@
     window.document.body.addEventListener('mouseup', () => {
         let selectedText = window.getSelection().toString()
         if (selectedText !== '') {
+            
             foo(selectedText, en)
             //  foo(selectedText,cj)
             //  foo(selectedText,jc)
@@ -120,28 +123,42 @@
             method: "GET",
             url: url,
             onload: function (response) {
-                let result = response.responseText
+                let result_1='', result_2='', result_3='';
+
+                result_1 = response.responseText
                     .replace(/\r\n/g, '')
                     .replace(/\n/g, '')
 
+                result_2 = result_1
+                    .replace(/<aside class="side">.+<\/aside>/gm, '')
+                    .match(/<section class="content">.+<\/section>/gm)[0]
+                    .replace(/<script type="text\/javascript">.+<\/script>/gm, '')
+                    .replace(/<div class="word-details-ads-placeholder"><\/div>/gm, '<div id="word-nav">' + result_1.match(/<ul class="word-nav">.+?<\/ul>/gm) + '<span class="word-nav-point" data-point = "1" style="margin-left:10px;"></span></div>')
+                    .replace(/<ul class="word-nav">.+?<\/ul>/, '')
+                    .replace(/<ul class="word-nav">/, '<ul class="word-nav" style="margin-left:0px;">')
+                    .replace(/<div class="word-details-ads">.+?<\/div>\s.<\/div>/gm, '')
+                    .replace(/<div class="word-details-pane-content"/gm, '<div class="word-details-pane-content" style="margin-top:36px";')
+                    .replace(/(<h2>网络热点<\/h2>)|(<h2>详细释义<\/h2>)|(<h2>常用短语<\/h2>)|(<h2>英英释义<\/h2>)|(<h2>词形变化<\/h2>)|(<h2>同反义词<\/h2>)|(<h2>词义辨析<\/h2>)/gm, '')
+                    .replace(/<a\shref="\/w\//gm, '<a data-syn-word="')
+                    .replace(/http:\/\//gm, 'https://')
+                    .replace(/class="content"/g, 'class="translateD-content"')
+                
                 // 写入内容
-                document.querySelector('#translateD').innerHTML = (
-                    result
-                        .replace(/<aside class="side">.+<\/aside>/gm, '')
-                        .match(/<section class="content">.+<\/section>/gm)[0]
-                        .replace(/<script type="text\/javascript">.+<\/script>/gm, '')
-                        .replace(/<div class="word-details-ads-placeholder"><\/div>/, '<div id="word-nav">' + result.match(/<ul class="word-nav">.+?<\/ul>/gm) + '<span class="word-nav-point" data-point = "1" style="margin-left:10px;"></span></div>')
-                        .replace(/<ul class="word-nav">.+?<\/ul>/, '')
-                        .replace(/<ul class="word-nav">/, '<ul class="word-nav" style="margin-left:0px;">')
-                        .replace(/<div class="word-details-ads">.+?<\/div>\s.<\/div>/gm, '')
-                        .replace(/<div class="word-details-pane-content".+<\/div>(?=\s.+<\/section>\s.)/gm, '<div id="word-details-pane-content">' + result.match(/<div class="word-details-pane-content".+<\/div>(?=\s.+<\/section>\s.)/gm) + '</div>')
-                        .replace(/<div class="word-details-pane-content"/gm, '<div class="word-details-pane-content" style="margin-top:36px";')
-                        .replace(/(<h2>网络热点<\/h2>)|(<h2>详细释义<\/h2>)|(<h2>常用短语<\/h2>)|(<h2>英英释义<\/h2>)|(<h2>词形变化<\/h2>)|(<h2>同反义词<\/h2>)|(<h2>词义辨析<\/h2>)/gm, '')
-                        .replace(/<a\shref="\/w\//gm, '<a data-syn-word="')
-                        .replace(/http:\/\//gm, 'https://')
-                        .replace(/class="content"/g, 'class="translateD-content"')
-                        .replace(/<footer.+<\/footer>/gm, '')
-                )
+
+                let m = /<div class="word-details-pane-content".+?<\/footer>/
+                if (new RegExp(m,'gm').test(result_2)) {                  
+                    result_2.match(m).forEach(e => {
+                     
+                         result_3 = result_2.replace(m, '<div class="word-details-pane-content-all">' + e + '</div>')
+                    })
+                }else{
+                     result_3 = result_2
+                }
+                
+                document.querySelector('#translateD').innerHTML = result_3.replace(/<footer class="word-details-pane-footer">.+?<\/footer>/gm,'')
+                
+                //console.log(res)
+
 
 
 
@@ -235,14 +252,18 @@
 
                 let wordDetail = function () {
                     // 2.content detail
-                    const wordDetailsId = document.querySelector('#word-details-pane-content')
+                    const wordDetailsId = document.querySelector('.word-details-pane-content')
                     const wordDetailsClass = document.querySelector('.word-details-pane-content')
 
                     let wordDetailHeight
+                    let wordHeight = 0;
                     const formHeight = parseInt(getComputedStyle(document.querySelector('#translateD'), null).height.replace(/px/, ''))
                     const headerHeight = parseInt(getComputedStyle(document.querySelector('.word-details-pane-header'), null).height.replace(/px/, ''))
                     const navHeight = parseInt(getComputedStyle(document.querySelector('.word-nav'), null).height.replace(/px/, ''))
-
+                    if(document.getElementsByClassName('.word-details-header')){
+                        // 
+                        wordHeight = parseInt(getComputedStyle(document.querySelector('.word-details-header'), null).height.replace(/px/, ''))
+                    }
                     let detailHeight
 
                     const scollDistance = 120
@@ -273,7 +294,7 @@
                         }
                         // 内容高度 - div 高度
                         else if (event.deltaY > 0) {
-                            detailHeight = formHeight - headerHeight - navHeight
+                            detailHeight = formHeight - headerHeight - navHeight - wordHeight
 
                             if (wordDetailHeight > detailHeight) {
                                 if (-(wordDetailHeight - detailHeight) > wordDetailsClassMarginTop - 120) {
